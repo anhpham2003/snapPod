137e55f2d3b671448bd90d364868ddfd0a08913a
Anh Pham <anhsmacbook@Anhs-MacBook-Pro.local>
2025-09-05 16:37:17 -0700

    fix(dashboard): fallback filename when displayName is empty; use || instead of ??

---
+++ snappod-podcast-clipper-frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,58 @@
+"use server";
+
+import { auth } from "~/server/auth";
+import { redirect } from "next/navigation";
+import { db } from "~/server/db";
+import { DashboardClient } from "~/components/dashboard-client";
+
+
+export default async function DashboardPage() {
+    const session = await auth();
+
+    if (!session?.user?.id) {
+        redirect("/login");
+    }
+
+    await new Promise((res) => setTimeout(res, 5000))
+
+    const userData = await db.user.findUniqueOrThrow({
+        where: { id: session.user.id },
+        select: {
+            uploadedFiles: {
+                where: {
+                    uploaded: true,   
+                },
+                select: {
+                    id: true,
+                    s3Key: true,
+                    displayName: true,
+                    status: true,
+                    createdAt: true,
+                    _count: {
+                        select: {
+                            clips: true,
+                        },
+                    },
+                },
+            },
+            clips: {
+                orderBy: {
+                    createdAt: "desc",
+                }
+            }
+        },
+    });
+
+    const formattedFiles = userData.uploadedFiles.map((file) => ({
+        id: file.id,
+        s3Key: file.s3Key,
+        filename: file.displayName || "Unknown file name",
+        status: file.status,
+        clipsCount: file._count.clips,
+        createdAt: file.createdAt
+    }))
+
+    return (
+    <DashboardClient uploadedFiles={formattedFiles} clips ={userData.clips} />
+    )
+}
